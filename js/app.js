a3a0cbfa1fa2a2e8baaa266b443e7261ad57c1fd
Garrett Bryan <garrettdavisbryan@gmail.com>
2014-12-21 18:39:56 -0500

    align enemy xposition to foot position

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(positionX, positionY, speedX) {
+var Enemy = function(positionX, positionY, speedX, scale) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.sX = speedX;
