6f3e5212677a5ee535e3e2e9364273916c36138d
Garrett Bryan <garrettdavisbryan@gmail.com>
2014-12-21 22:19:49 -0500

    add visual collision circles to verify positioning

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function(positionX, positionY, speedX, scale) {
+var Enemy = function(positionX, positionY, speed, scale) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.sX = speedX;
-    this.x = positionX;
-    this.y = positionY;
+    this.speed = speed;
+    this.scale = scale;
+    this.x = positionX - 101/2 * this.scale;
+    this.y = positionY - 135 * this.scale;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -16,37 +17,49 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.sX * dt;
-    if (this.x > 5 * 101){
-        this.x = -1 * 101;
-        this.y = Math.floor((Math.random()*3)+1) * 83 - 20;
-        this.sX = Math.random()*80 + 20;
-    }
+
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.scale(1,1);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 101 * this.scale, 171 * this.scale);
+    ctx.beginPath();
+    ctx.arc(this.x + (101/2 + 20) * this.scale, this.y + 108 * this.scale, 30 * this.scale, 0, 2 * Math.PI, false);
+    //ctx.fillStyle = rgba(0,0,0, 126);
+    ctx.fill();
  //   ctx.scale(0.25,0.25);
 };
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(positionX, positionY, speed){
-    this.s = speed;
-    this.x = positionX;
-    this.y = positionY;
+var Player = function(positionX, positionY, speed, scale){
+    this.speed = speed;
+    this.scale = scale;
+    this.x = positionX - 101/2 * this.scale;
+    this.y = positionY - 135 * this.scale;
     this.sprite = 'images/char-boy.png';
 };
+
+Player.prototype.footPosition = function(postionX, positionY){
+
+}
 
 Player.prototype.update = function() {
 
 };
 
+
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y, 101 * this.scale, 171 * this.scale);
+    ctx.beginPath();
+    ctx.arc(this.x + (101/2 + 1) * this.scale, this.y + 125 * this.scale, 15 * this.scale, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc(this.x + (101/2 + 1) * this.scale, this.y + 95 * this.scale, 33 * this.scale, 0, 2 * Math.PI, false);
+    ctx.fill();
 };
 
 Player.prototype.handleInput = function() {
@@ -63,13 +76,14 @@ var allEnemies = [];
 
 for (var i = 0; i < 3; i++){
     allEnemies.push(function(){
-        return new Enemy( -1 * 101, i*83, 100);
+        return new Enemy( 330, 200 + 80 * 1, 0, 1);
     }());
     console.log(allEnemies[i]);
 }
 
 
-var player = new Player(4 * 101, 5 * 83-40, 5);
+var player = new Player( -101/2 + 101 * 4, 200 + 80 * 2, 5, 1);
+console.log(player);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
